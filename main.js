c8475255ca60f6090f6b5648a017e5e44bbad242
Sven Huff <info@sven-huff.de>
2021-01-05 15:25:59 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let message = document.getElementById("email");
+let inputButton = document.getElementById("inputButton");
+
+inputButton.addEventListener("click", alert("hi"));
+
+// let valid = document.getElementById("email").validity.valid;
